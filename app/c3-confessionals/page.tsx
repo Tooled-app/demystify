6978fa93c92474efbbcbfe9f98dca3c15dcece28
Seereusone <signinguptononsense@gmail.com>
2026-05-18 14:20:15 +0100

    fix: add null check to confessionals page

--- app/c3-confessionals/page.tsx
+++ app/c3-confessionals/page.tsx
@@ -15,6 +15,7 @@ export default async function ConfessionalsPage() {
 
         <div className="confessional-grid">
           {confessionals.map((conf) => {
+            if (!conf) return null;
             const colour = getConfessionalColour(conf.date);
             return (
               <Link
